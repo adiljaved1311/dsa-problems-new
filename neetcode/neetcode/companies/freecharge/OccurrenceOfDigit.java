bc06f1ac06d64dfc2a9d30d61284d8a3ad6617a6
Adil Javed <63340527+adiljaved1311@users.noreply.github.com>
2022-09-25 22:55:07 +0530

    Delete neetcode/neetcode directory

--- neetcode/neetcode/companies/freecharge/OccurrenceOfDigit.java
+++
@@ -1,9 +0,0 @@
-package neetcode.companies.freecharge;
-
-//WAP to find the occurrence of a digit in array
-
-public class OccurrenceOfDigit {
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,2,5};
-    }
-}
